900d490555efabf8c1d7db62f0d49accd51ff6d5
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-05-05 20:57:44 +0900

    ローカル通知

--- taskapp/application/services/TaskService.swift
+++ taskapp/application/services/TaskService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UserNotifications
 
 class TaskService {
     private let taskRepository: TaskRepository
@@ -35,13 +36,15 @@ class TaskService {
         task.content = content
         task.date = date
 
-        let taskId = (self.taskList.last?.id ?? 0) + 1
+        let taskId = ((self.taskList.max(by: { (a, b) -> Bool in return a.id < b.id }))?.id ?? 0) + 1
         task.id = taskId
 
         let result = self.taskRepository.create(task: task)
         if result {
             self.fetchAllTaskList()
         }
+        
+        setTaskNotification(task)
         
         return result
     }
@@ -62,15 +65,23 @@ class TaskService {
             self.fetchAllTaskList()
         }
         
+        setTaskNotification(task)
+        
         return result
     }
     
     func deleteTaskAt(index: Int) -> Bool {
         let task = self.taskList[index]
+        let taskId = task.id
         let result = self.taskRepository.delete(task: task)
         if result {
             self.fetchAllTaskList()
         }
+        
+        // 通知の削除
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [String(taskId)])
+        printPendingNotifications(center)
         
         return result
     }
@@ -81,5 +92,35 @@ class TaskService {
     
     func getTaskCount() -> Int {
         return taskList.count
+    }
+    
+    func setTaskNotification(_ task: Task) {
+        let content = UNMutableNotificationContent()
+        content.title = task.title
+        content.body = task.content.isEmpty ? "（内容なし）" : task.content
+        content.sound = UNNotificationSound.default
+        
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { error in
+            print(error ?? "ローカル通知登録OK")
+        }
+        
+        printPendingNotifications(center)
+    }
+    
+    private func printPendingNotifications(_ center: UNUserNotificationCenter) {
+        center.getPendingNotificationRequests { requests in
+            for request in requests {
+                print("/-------------------/")
+                print(request)
+                print("/-------------------/")
+            }
+        }
     }
 }
